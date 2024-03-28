9e77e0ed8cb5ab6af5415ae9470d0b9c05443299
JA <82273499+swimmingHaribo@users.noreply.github.com>
2024-03-27 18:27:51 -0700

    add image_to_text model and images

---
+++ backend/python/image_to_text.py
@@ -0,0 +1,49 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from PIL import Image
+import os
+
+# https://huggingface.co/vikhyatk/moondream2
+model_id = "vikhyatk/moondream2"
+revision = "2024-03-06"
+model = AutoModelForCausalLM.from_pretrained(
+    model_id, trust_remote_code=True, revision=revision
+)
+tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
+
+
+def process_images():
+    # get the path of the current py file
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    # append 'images' to get to the images folder
+    img_folder = os.path.join(current_dir, 'images')
+
+    # question = "Describe this image in detail."
+    question = "Describe this image with detailed context, including objects, people, background elements, colors, textures, emotions, and any symbols or text present."
+    description_file = os.path.join(current_dir, 'description.txt')
+
+    # check if the folder exists
+    if not os.path.exists(img_folder):
+        print(f"Folder {img_folder} does not exist.")
+        return
+
+    # open the description file for writing
+    with open(description_file, 'w') as file:
+        # iterate over each file in the image folder
+        for filename in os.listdir(img_folder):
+            if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+                try:
+                    # get the path to the image
+                    file_path = os.path.join(img_folder, filename)
+                    # load the image
+                    image = Image.open(file_path)
+                    # encode the image using the model
+                    enc_image = model.encode_image(image)
+                    # ask the model a question about the image
+                    answer = model.answer_question(enc_image, question, tokenizer)
+                    # write the answer to the description file
+                    file.write(f"{answer}\n\n")
+                except Exception as e:
+                    print(f"Failed to process {filename}: {e}")
+
+
+process_images()
