d2607b998673271da4c1597c62d81a34ab477da4
scott0929 <scott0929@gmail.com>
2024-04-12 21:35:16 -0700

    removing python files

--- backend/python/a.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from PIL import Image
-import os
-
-
-if __name__ == '__main__':
-    print("python shell worked with imports")
--- backend/python/a2.py
+++
@@ -1,7 +0,0 @@
-import json
-import os
-from diffusers import DiffusionPipeline
-import torch # if using cuda
-
-if __name__ == '__main__':
-    print("python shell worked with imports")
